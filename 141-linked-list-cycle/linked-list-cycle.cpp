2a3bbf448329f052144550533de5d2079bace6ff
Prabhat_Jha <160613316+Prabhatjha-07@users.noreply.github.com>
2026-09-12 18:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (22.13%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,15 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
-        unordered_set<ListNode*> visited;
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            if (visited.count(curr)) {
+        // unordered_set<ListNode*> visited;
+        // ListNode* curr = head;
+        // while (curr != nullptr) {
+        //     if (visited.count(curr)) {
+        //         return true;
+        //     }
+
+        //     visited.insert(curr);
+        //     curr = curr -> next;
+        // }
+        // return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
                 return true;
             }
-
-            visited.insert(curr);
-            curr = curr -> next;
         }
         return false;
     }
