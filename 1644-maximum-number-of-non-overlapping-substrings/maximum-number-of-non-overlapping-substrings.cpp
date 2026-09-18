2bd04b9874671225c5b21b3ef180b26fa664083f
Prabhat_Jha <160613316+Prabhatjha-07@users.noreply.github.com>
2026-09-18 12:56:04 +0530

    Time: 6 ms (86.93%) | Memory: 23.3 MB (88.07%) - LeetSync

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.cpp
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.cpp
@@ -1,47 +1,45 @@
-class Solution {
-public:
-    vector<string> maxNumOfSubstrings(string s) {
-        int n = s.length() ;
-        vector<int> start(26, -1);
-        vector<int> end(26);
-        vector<bool> isValid(26 , true);
-        vector<string> result;
+class Solution { 
+public: 
+    vector<string> maxNumOfSubstrings(string s) { 
+        int n = s.length(); 
+        vector<pair<int, int>> range(26, {-1, -1}); 
+        vector<bool> isValid(26, true); 
+        vector<string> result; 
+ 
+        for (int i = 0; i < n; i++) { 
+            int idx = s[i] - 'a'; 
+ 
+            if (range[idx].first == -1) { 
+                range[idx].first = i; 
+            } 
+            range[idx].second = i; 
+        } 
+         
+        for(int c = 0; c < 26; c++){ 
+            if(range[c].first == -1) continue; 
+ 
+            for(int i = range[c].first; i <= range[c].second; i++){ 
+                if(range[s[i] - 'a'].first < range[c].first){ 
+                    isValid[c] = false; 
+                    break; 
+                } 
+                range[c].second = max(range[c].second, range[s[i] - 'a'].second); 
+            } 
+        } 
 
-        for (int i = 0; i < n; i++) {
-            int idx = s[i] - 'a';
-
-            if (start[idx] == -1) {
-                start[idx] = i;
-            }
-            end[idx] = i;
-        }
-        
-
-        for(int c = 0 ; c < 26 ; c++){
-            if(start[c] == -1) continue;
-
-            for(int i = start[c] ; i <= end[c] ; i++){
-                if(start[s[i] - 'a'] < start[c]){
-                    isValid[c] = false;
-                    break;
-                }
-                end[c] = max(end[c] , end[s[i] - 'a']);
-            }
-        }
-        int lastsubstr = INT_MAX;
-
-        for(int i = n-1; i >= 0 ; i--){
-            int c = s[i] - 'a';
-
-            if(! isValid[c] )continue;
-
-            if(i == start[c] && end[c] <  lastsubstr ){
-                result.push_back(s.substr(i , end[c] - i + 1 ));
-                lastsubstr = i;
-
-            }
-
-        }
-        return result;
-    }
+        int lastsubstr = INT_MAX; 
+ 
+        for(int i = n-1; i >= 0; i--){ 
+            int c = s[i] - 'a'; 
+ 
+            if(!isValid[c]) continue; 
+ 
+            if(i == range[c].first && range[c].second < lastsubstr){ 
+                result.push_back(s.substr(i, range[c].second - i + 1)); 
+                lastsubstr = i; 
+            } 
+        } 
+ 
+        return result; 
+    } 
 };
